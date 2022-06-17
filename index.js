48000459ab0c452b17adc430642feec072b8b151
Diego Siqueira <diego.camomila@gmail.com>
2022-06-17 19:05:19 -0300

    feat: iniciando desafio 1, retornar um array com todas as pessoas palestrantes cadastradas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +12,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = JSON.parse(await fs.readFile('./talker.json'));
+  if (data.length === 0) return res.status(200).json([]);
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
