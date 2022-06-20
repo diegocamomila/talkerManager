5f11117b28d5e0ee9ee94dfa47c1078b0e382ddd
Diego Siqueira <diego.camomila@gmail.com>
2022-06-20 19:24:51 -0300

    feat: criado o endpoint get /talker/:id

--- index.js
+++ index.js
@@ -14,11 +14,34 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => {
+// 1 - Crie o endpoint GET /talker
+app.get('/talker', async (_req, response) => {
   const data = JSON.parse(await fs.readFile('./talker.json'));
-  if (data.length === 0) return res.status(200).json([]);
-  res.status(200).json(data);
+  if (data.length === 0) return response.status(200).json([]);
+  response.status(200).json(data);
 });
+// 1 final
+
+// 2 inicio - Crie o endpoint GET /talker/:id
+const getData = async (request, _res, next) => {
+  const data = JSON.parse(await fs.readFile('./talker.json'));
+  request.data = data;
+  next();
+};
+
+app.get('/talker/:id', getData, async (request, response) => {
+  const { data } = request;
+  const { id } = request.params;
+
+  const findTalker = data.find((talker) => talker.id === Number(id));
+    if (!findTalker) {
+      return response.status(404).json(
+        { message: 'Pessoa palestrante não encontrada' },
+      ); 
+  } 
+  response.status(200).json(findTalker);
+});
+// 2 final
 
 app.listen(PORT, () => {
   console.log('Online');
