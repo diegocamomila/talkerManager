094f4bf89a4ab19cd78c75e338edf921360fb7e1
Diego Siqueira <diego.camomila@gmail.com>
2022-06-21 21:34:27 -0300

    feat:7 - Crie o endpoint DELETE /talker/:id

--- routes/routerTalker.js
+++ routes/routerTalker.js
@@ -53,4 +53,12 @@ check.checkRate,
   res.status(200).json({ id: +id, ...req.body });
 });
 
+routerTalker.delete('/:id', check.checkToken, async (req, res) => {
+  const { id } = req.params;
+  const talker = await getTalker();
+  const filterTalker = talker.filter((t) => t.id !== +id);
+  postTalker(filterTalker);
+  res.status(204).end();
+});
+
 module.exports = routerTalker;
